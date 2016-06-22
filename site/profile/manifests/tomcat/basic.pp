e1fca763e5369ee7cb2098854d2970e5af20d352
Ruth Linehan <ruth@puppetlabs.com>
2016-06-22 11:25:59 -0700

    Update tomcat version
    Because 7.0.69 URL returned a 404.

--- site/profile/manifests/tomcat/basic.pp
+++ site/profile/manifests/tomcat/basic.pp
@@ -1,7 +1,7 @@
 class profile::tomcat::basic {
   include ::java
   ::tomcat::install { '/opt/tomcat':
-    source_url => 'http://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz',
+    source_url => 'http://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.70/bin/apache-tomcat-7.0.70.tar.gz',
   }
   ::tomcat::instance { 'default':
     catalina_home => '/opt/tomcat',
