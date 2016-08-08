6d26d332b5898d702bb19a734527f68d779f5d51
Jeremy Barlow <jeremy.barlow@puppet.com>
2016-08-08 10:38:00 -0700

    (MAINT) Re-added node resources to site.pp for classification
    This commit readds node classifications to the site.pp file, for use in
    testing catalog compilation with the puppetserver-memmeasure tool.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,11 @@
-#node 'default' {
-  #include ::role::by_size::empty
-  #include ::role::by_size::small
-  #include ::role::by_size::medium
-#}
+node 'small' {
+  include ::role::by_size::small
+}
+
+node 'empty' {
+  include ::role::by_size::empty
+}
+
+node 'medium' {
+  include ::role::by_size::medium
+}
