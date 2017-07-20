eb3144499a509c1c1a3a9d5ee6a1346ba0f5a978
Nick Walker <nick.walker@puppetlabs.com>
2017-07-20 11:35:42 -0700

    (PERF-201) Add a manifest to create file_lines and ini_settings
    This manifest will create a completely arbitrary file from
    file_line resources and ini_settings resources.  The purpose is just
    to add 100 of each resource into the catalog.

--- site/role/manifests/by_size/large.pp
+++ site/role/manifests/by_size/large.pp
@@ -4,4 +4,5 @@ class role::by_size::large {
   include ::profile::apache::basic
   include ::profile::influxdb::basic
   include rampup_profile_gitlab
+  include ::profile::loop_through_file_resources
 }
